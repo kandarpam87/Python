6cc64ae8cc12b73107445b88000b90b6337cd7ff
Kandarpa Malipeddi <kandarpa_malipeddi@persistent.co.in>
2020-05-28 11:12:08 +0530

    added Upper example and triplequote exmple.

--- StringExamples.py
+++ StringExamples.py
@@ -1,12 +1,27 @@
+
+'''
+ClassName.  -> this will gives the list of available attributes and functions for the class
+
+Ex: str.
+'''
+
 s1 = "Welcome to PSL"
-print(s1.find("PSL")) #Retruns 11
-print(s1.find("123")) #Retruns -1
+
+print("s1 = ", s1, ", id(s1) = ", id(s1))
+
+print("""s1.find("PSL") = """, s1.find("PSL")) #Retruns 11
+print("""s1.find("123") = """, s1.find("123")) #Retruns -1
 
 '''
 print(s1.index("psl")) #Gives ValueError
 '''
 
-#Repeate operator
+#Repeate operator 
 # ""*number
 
 print("-"*40)
+
+    
+s2 = s1.upper()
+print("s1 = ", s1, ", id(s1) = ", id(s1))
+print("s2 = ", s2)
